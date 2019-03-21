53a3d4ed533d45040e3def72e03a2e367afaaaa0
jeremy yvinec <jeremy29410@gmail.com>
2019-03-21 11:28:31 +0100

    bar footer

--- resources/views/layouts/barFooterMobile.blade.php
+++ resources/views/layouts/barFooterMobile.blade.php
@@ -1,13 +1,13 @@
-    <div class="bar_mobile">
-        <div>
+    <div class="bar_mobile row">
+        <div class="col">
             <a href="{{ route('home') }}"><svg class="svg"><use xlink:href="{{ asset('svg/sprite.svg#noun_Home_671857') }}"/></svg></a>
             <p>Accueil</p>
         </div>
-        <div>
+        <div class="col">
             <a href="{{ route('decouvrir') }}"><svg class="svg"><use xlink:href="{{ asset('svg/sprite.svg#noun_percentage_1045094') }}"/></svg></a>
-            <p>Mes promotions</p>
+            <p>Promotions</p>
         </div>
-        <div>
+        <div class="col">
             <a href="{{ route('decouvrir') }}"><svg class="svg"><use xlink:href="{{ asset('svg/sprite.svg#noun_discover_1666932') }}"/></svg></a>
             <p>Découvrir</p>
         </div>
