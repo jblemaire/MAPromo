24a1a8f83ae78390f96c38aeb478e18645af70d2
jeremy yvinec <jeremy29410@gmail.com>
2019-03-24 14:18:17 +0100

    magasin card

--- resources/views/responsable/magasins.blade.php
+++ resources/views/responsable/magasins.blade.php
@@ -15,87 +15,87 @@
                     {{ csrf_field() }}
 
                     <div class="form-group">
-                        <label for="nomMag" class="col-md-4 control-label">Nom du Magasin*</label>
+                        <label for="nomMag">Nom du Magasin*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="nomMag" type="text" class="inputText" name="nomMag" value="{{ old('nomMag') }}" required autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="siretMag" class="col-md-4 control-label">SIRET*</label>
+                        <label for="siretMag">SIRET*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="siretMag" type="text" class="inputText" name="siretMag" value="{{ old('siretMag') }}" required autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="adresse1Mag" class="col-md-4 control-label">N° Rue</label>
+                        <label for="adresse1Mag">N° Rue</label>
 
-                        <div class="col-md-1">
+                        <div>
                             <input id="adresse1Mag" type="text" class="inputText" name="adresse1Mag" value="{{ old('adresse1Mag') }}" autofocus>
                         </div>
 
-                        <label for="adresse2Mag" class="col-md-1 control-label">Adresse*</label>
+                        <label for="adresse2Mag" class="control-label">Adresse*</label>
 
-                        <div class="col-md-4">
+                        <div>
                             <input id="adresse2Mag" type="text" class="inputText" name="adresse2Mag" value="{{ old('adresse2Mag') }}" required autofocus>
                         </div>
 
                     </div>
 
                     <div class="form-group">
-                        <label for="cpMag" class="col-md-4 control-label">CP*</label>
+                        <label for="cpMag">CP*</label>
 
-                        <div class="col-md-1">
+                        <div>
                             <input id="cpMag" type="text" class="inputText" name="cpMag" value="{{ old('cpMag') }}" onkeyup="getVilleByCp(this.value)" required autofocus>
                         </div>
 
-                        <label for="villeMag" class="col-md-1 control-label">Ville*</label>
+                        <label for="villeMag" class="control-label">Ville*</label>
 
-                        <div class="col-md-4">
+                        <div>
                             <select class="form-control custom-select" aria-label="Ville" aria-describedby="button-addon2" name="villeMag" id="villeMag" onchange="getCoordonnes()" required disabled>
                             </select>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="latMag" class="col-md-4 control-label">Latitude*</label>
+                        <label for="latMag">Latitude*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="latMag" type="text" class="inputText" name="latMag" value="{{ old('latMag') }}" required readonly autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="longMag" class="col-md-4 control-label">Longitude*</label>
+                        <label for="longMag">Longitude*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="longMag" type="text" class="inputText" name="longMag" value="{{ old('longMag') }}" required readonly autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="mailMagasin" class="col-md-4 control-label">Email*</label>
+                        <label for="mailMagasin">Email*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="mailMagasin" type="email" class="inputText" name="mailMagasin" value="{{ old('mailMagasin') }}" required autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="telMag" class="col-md-4 control-label">Téléphone</label>
+                        <label for="telMag">Téléphone</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="telMag" type="text" class="inputText" name="telMag" value="{{ old('telMag') }}" autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="selectType" class="col-md-4 control-label">Type*</label>
+                        <label for="selectType">Type*</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <select class="form-control custom-select" aria-label="Type" aria-describedby="button-addon2" id="selectType" name="selectType" onchange="searchCategories()">
                                 <option value="">--Choisir un type--</option>
                                 @foreach($types as $type)
@@ -106,9 +106,9 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="selectCategorie" class="col-md-4 control-label">Categorie</label>
+                        <label for="selectCategorie">Categorie</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <select class="form-control custom-select" aria-label="Categorie" aria-describedby="button-addon2" name="selectCategorie" id="selectCategorie" disabled>
                                 <option value="">--Choisir une categorie--</option>
                             </select>
@@ -116,17 +116,17 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="photo1Mag" class="col-md-4 control-label">Photo 1</label>
+                        <label for="photo1Mag">Photo 1</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="photo1Mag" type="file" class="inputText" name="photo1Mag" value="{{ old('photo1Mag') }}" accept=".jpg, .jpeg, .png" autofocus>
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <label for="photo2Mag" class="col-md-4 control-label">Photo 2</label>
+                        <label for="photo2Mag">Photo 2</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="photo2Mag" type="file" class="inputText" name="photo2Mag" value="{{ old('photo2Mag') }}" accept=".jpg, .jpeg, .png" autofocus>
                         </div>
                     </div>
